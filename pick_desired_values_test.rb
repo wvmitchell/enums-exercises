678e570e8fe65888260ab4edf03a64ec2620e140
Will Mitchell <wvmitchell@gmail.com>
2013-09-18 13:28:08 -0600

    Finished pick_desired_values using select

--- pick_desired_values_test.rb
+++ pick_desired_values_test.rb
@@ -5,59 +5,67 @@ require 'minitest/pride'
 class PickDesiredValuesTest < Minitest::Test
   def test_pick_even_numbers
     numbers = (1..10)
-    evens = []
-    numbers.each do |number|
-      # write code here
+    evens = numbers.select do |number|
+      number.even?
     end
     assert_equal [2, 4, 6, 8, 10], evens
   end
 
   def test_pick_odd_numbers
-    skip
     numbers = (1..10)
-    # write code here
+    odds = numbers.select do |number|
+      number.odd?
+    end
     assert_equal [1, 3, 5, 7, 9], odds
   end
 
   def test_pick_words_with_three_letters
-    skip
     words = %w(pill bad finger cat blue dog table red)
-    # write code here
+    selected = words.select do |word|
+      word.length == 3
+    end
     assert_equal %w(bad cat dog red), selected
   end
 
   def test_pick_words_with_more_than_three_letters
-    skip
     words = %w(pill bad finger cat blue dog table red)
-    # write code here
+    selected = words.select do |word|
+      word.length > 3
+    end
     assert_equal %w(pill finger blue table), selected
   end
 
   def test_pick_words_ending_in_e
-    skip
     words = %w(are you strike thinking belt piece warble sing pipe)
-    # write code here
+    selected = words.select do |word|
+      word[-1] == 'e'
+    end
     assert_equal %w(are strike piece warble pipe), selected
   end
 
   def test_pick_words_ending_in_ing
-    skip
     words = %w(bring finger drought singing hat shoehorning purposeful)
-    # write code here
-    assert_equal %w(singing shoehorning), selected
+    selected = words.select do |word|
+      word[-3..-1] == 'ing'
+    end
+    assert_equal %w(bring singing shoehorning), selected
   end
 
   def test_select_all_colors
-    skip
     words = %w(four red green blue pizza purple)
-    # write code here
+    colors = %w(red green blue purple)
+    selected = words.select do |word|
+      colors.include?(word)
+    end
     assert_equal %w(red green blue purple), selected
   end
 
   def test_pick_dinosaurs
-    skip
     animals = %w(tyrannosaurus teacup_poodle achillesaurus ankylosaurus qingxiusaurus)
-    #write code here
+    not_animal = 'teacup_poodle'
+    selected = animals.select do |animal|
+      animal != not_animal
+    end
     assert_equal %w(tyrannosaurus achillesaurus ankylosaurus qingxiusaurus), selected
   end
 end
