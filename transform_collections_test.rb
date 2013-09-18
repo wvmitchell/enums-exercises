2daafc47701865d961aa89685efaf054470508bf
Will Mitchell <wvmitchell@gmail.com>
2013-09-18 13:17:52 -0600

    Finished transform_collections_test with collect

--- transform_collections_test.rb
+++ transform_collections_test.rb
@@ -5,57 +5,65 @@ require 'minitest/pride'
 class TransformCollectionsTest < Minitest::Test
   def test_capitalize
     names = %w(alice bob charlie)
-    capitalized_names = []
-    # write code here
+    capitalized_names = names.collect do |name|
+      name.capitalize
+    end
     assert_equal %w(Alice Bob Charlie), capitalized_names
   end
 
   def test_doubles
-    skip
     numbers = [1, 2, 3, 4, 5]
-    # write code here
+    doubles = numbers.collect do |number|
+      number*2
+    end
     assert_equal [2, 4, 6, 8, 10], doubles
   end
 
   def test_squares
-    skip
     numbers = [1, 2, 3, 4, 5]
-    # write code here
+    squares = numbers.collect do |number|
+      number**2
+    end
     assert_equal [1, 4, 9, 16, 25], squares
   end
 
   def test_lengths
-    skip
     names = %w(alice bob charlie david eve)
-    # write code here
+    lengths = names.collect do |name|
+      name.length 
+    end
     assert_equal [5, 3, 7, 5, 3], lengths
   end
 
   def test_normalize_zip_codes
-    skip
     numbers = [234, 10, 9119, 38881]
-    # write code here
+    zip_codes = numbers.collect do |number|
+      number.to_s.rjust(5, "0")
+    end
     assert_equal %w(00234 00010 09119 38881), zip_codes
   end
 
   def test_reverse
-    skip
     names = %w(alice bob charlie david eve)
-    # write code here
+    backwards = names.collect do |name|
+      name.reverse 
+    end
     assert_equal %w(ecila bob eilrahc divad eve), backwards
   end
 
   def test_words_with_no_vowels
-    skip
     words = %w(green sheep travel least boat)
-    # write code here
+    without_vowels = words.collect do |word|
+      word.tr('aeiouy', "")
+    end
     assert_equal %w(grn shp trvl lst bt), without_vowels
   end
 
   def test_trim_last_letter
-    skip
     animals = %w(dog cat mouse frog platypus)
-    #write code here
+    trimmed = animals.collect do |animal|
+      animal[0..-2]
+    end
     assert_equal %w(do ca mous fro platypu), trimmed
   end
 
